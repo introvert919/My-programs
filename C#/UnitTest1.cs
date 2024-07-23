585cf980e3c4c645c427066bdcae8e0e1cfcd391
Andrey Fatkulin <146550720+introvert919@users.noreply.github.com>
2024-07-23 15:32:15 +0300

    Add files via upload

---
+++ C#/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+
+namespace Тест_Nunit_для_Jenkins
+{
+
+    [TestFixture]
+
+        public class UnitTest1
+        {
+
+        [Test]
+        public void TestMethod1()
+        {
+            string a = "a";
+            string b = "a";
+
+            ClassicAssert.AreEqual(a, b);
+        }
+        }
+}
+
+
