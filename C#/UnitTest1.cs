8246a52a28b09ff079c47f4bb67c09a75e8c6e2a
Andrey Fatkulin <146550720+introvert919@users.noreply.github.com>
2024-08-14 14:08:40 +0300

    Add files via upload

--- C#/UnitTest1.cs
+++ C#/UnitTest1.cs
@@ -1,24 +1,48 @@
 ﻿using System;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
-
-
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Тест_Nunit_для_Jenkins
 {
 
     [TestFixture]
 
-        public class UnitTest1
-        {
+   
+      
 
-        [Test]
-        public void TestMethod1()
-        {
-            string a = "a";
-            string b = "a";
 
-            ClassicAssert.AreEqual(a, b);
+public class UnitTest1
+{
+
+    static HttpClient httpClient = new HttpClient();
+
+    [OneTimeSetUp]
+    public async Task Main()
+    {
+        
+       
+    }
+
+    [OneTimeTearDown]
+
+    public void Close_url()
+    {
+       httpClient.Dispose();
+    }
+
+    [Test]
+
+        public async Task TestMethod1()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com");
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            string Actual = response.StatusCode.ToString();
+            string Expected = "OK";
+
+            ClassicAssert.AreEqual(Expected, Actual);
         }
         }
 }
